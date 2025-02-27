5e55a380ca9640c24eddfb07de1552e4938128fc
Jean <dev.jeancotillo@gmail.com>
2025-02-27 19:40:29 -0300

    first commit

---
+++ src/academy/devdojo/maratonajava/introduction/Main.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("JKA-ME-HA-ME-HAAA");
+    }
+}
