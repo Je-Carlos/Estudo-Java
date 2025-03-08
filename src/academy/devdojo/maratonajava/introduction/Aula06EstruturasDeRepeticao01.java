3de08454f003e0bb1ad303b7daf020e20289afaa
Jean <dev.jeancotillo@gmail.com>
2025-03-07 21:57:52 -0300

    Estrutura de repetição

---
+++ src/academy/devdojo/maratonajava/introduction/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while (count < 5) {
+            System.out.println(++count);
+        }
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println("Eu repeti " + i + " vezes");
+        }
+    }
+}
