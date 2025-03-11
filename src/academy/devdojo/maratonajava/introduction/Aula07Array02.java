66a7e779b1ef79ce5268b3e0df624be50a425654
Jean <dev.jeancotillo@gmail.com>
2025-03-11 20:22:55 -0300

    arrays

---
+++ src/academy/devdojo/maratonajava/introduction/Aula07Array02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Aula07Array02 {
+    public static void main(String[] args) {
+        // valores padrão de inicialização
+        // short, byte, int, float, double = 0
+        // char = '\u0000' ''
+        // boolean = false
+        // String = null
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+
+    }
+}
