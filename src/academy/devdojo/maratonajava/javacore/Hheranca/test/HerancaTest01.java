a41d8c4a9f568ec3844056b562313e0301c42ae2
Je-Carlos <dev.jeancotillo@gmail.com>
2025-07-31 21:49:11 -0300

    Herança - construtores

--- src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -7,19 +7,17 @@ import academy.devdojo.maratonajava.javacore.Hheranca.domain.Pessoa;
 public class HerancaTest01 {
     public static void main(String[] args) {
         Endereco endereco = new Endereco();
-        Pessoa pessoa = new Pessoa();
+        Pessoa pessoa = new Pessoa("Astarion");
         endereco.setCep("12340-123");
         endereco.setRua("Rua Baldur");
 
-        pessoa.setNome("Astarion");
         pessoa.setCpf("12341234");
         pessoa.setEndereco(endereco);
 
         pessoa.imprime();
 
         System.out.println("------");
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Shadowheart");
+        Funcionario funcionario = new Funcionario("Shadowheart");
         funcionario.setCpf("43214321");
         funcionario.setEndereco(endereco);
         funcionario.setSalario(20000);
