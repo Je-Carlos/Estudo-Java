5d21bbdc7513f00a161fca28258f53d6766e7b66
Je-Carlos <dev.jeancotillo@gmail.com>
2025-08-02 19:10:15 -0300

    modificador final - tipo de referencia e tipo primitivo

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Comprador comprador2 = new Comprador();
+        carro.COMPRADOR.setNome("Pedro Pascal");
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+    }
+}
