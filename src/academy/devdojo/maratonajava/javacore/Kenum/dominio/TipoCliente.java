9ee2c42846a6ccd68a3dd1998dd94e49ef2c35a9
Jean Carlos <jeancotillo@proton.me>
2025-10-02 14:31:15 -0300

    Construtores e Atributos na enumeração

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,21 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private int VALOR;
+    private String NOME_RELATORIO;
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.VALOR = valor;
+        this.NOME_RELATORIO = nomeRelatorio;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    public String getNOME_RELATORIO() {
+        return NOME_RELATORIO;
+    }
 }
